6565956965b332f45f71dc84d03ef5656de40928
Peter Marks <peter.marks@gmail.com>
2019-09-22 18:18:18 -0400

    rename stuff

--- moar_dots/dot.py
+++ moar_dots/dot.py
@@ -9,32 +9,32 @@ class Dot:
     on the filesystem. Also acts as the parent class for all other file types.
     """
 
-    REQUIRED_ARGS = ['name', 'dotted', 'source', 'target']
-    OPTIONAL_ARGS = ['description']
-    DEFAULT_ARGS  = {"dotted":False}
+    REQUIRED_PROPERTIES = ['name', 'dotted', 'source', 'target']
+    OPTIONAL_PROPERTIES = ['description']
+    DEFAULT_PROPERTIES  = {"dotted":False}
 
     def __init__(self, config):
         self.log = logging.getLogger(__name__)
         self.log.debug("Initializing Dot object...")
 
         self.properties = {}
-        for required in self.REQUIRED_ARGS:
+        for required in self.REQUIRED_PROPERTIES:
             try:
                 self.properties['{required}'] = config.pop(required)
-            except KeyError as e:
+            except KeyError as key_error:
                 Wipe(
-                    e,
+                    key_error,
                     f"Tried to create a dot, but didn't have a {required} specified!",
                     f"Here's the remainder of the config being send to the Dot: {config}",
                 )
 
-        for optional in self.OPTIONAL_ARGS:
+        for optional in self.OPTIONAL_PROPERTIES:
             try:
                 self.properties['{optional}'] = config.pop(optional)
             except KeyError:
                 self.log.warn(f"No {optional} specified for {self.properties['name']}.")
 
-        for arg, default in self.DEFAULT_ARGS:
+        for arg, default in self.DEFAULT_PROPERTIES:
             try:
                 self.properties['{arg}'] = config.pop(arg)
             except KeyError:                
@@ -52,7 +52,7 @@ class Dot:
         """
         Installs the dotfile via symlink and backs up originals
         """
-        pass
+        self.log.info(f"DOT it: {self.properties['name']}")
 
     def nuke_it(self):
         """
