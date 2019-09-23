f7e975d09500ef6030068a98f8b9c80239f99bee
Peter Marks <peter.marks@gmail.com>
2019-09-23 00:44:12 -0400

    partial write-up of dot_it

--- moar_dots/constants.py
+++ moar_dots/constants.py
@@ -10,3 +10,5 @@ CONFIG_DIR = os.path.join(os.getenv("HOME"), ".config/moar-dots")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "config.yml")
 EASTER_FILE = os.path.join(CONFIG_DIR, ".easter.yml")
 ERROR_FILE = "moar_dots/data/error.yml"
+
+BACKUP_EXTENSION = ".moar-dots.bak"
--- moar_dots/dot.py
+++ moar_dots/dot.py
@@ -1,5 +1,7 @@
 import logging
+import os
 
+from .constants import BACKUP_EXTENSION
 from .wipe import Wipe
 
 
@@ -9,9 +11,9 @@ class Dot:
     on the filesystem. Also acts as the parent class for all other file types.
     """
 
-    REQUIRED_PROPERTIES = ['name', 'dotted', 'source', 'target']
+    REQUIRED_PROPERTIES = ['name', 'source', 'target']
     OPTIONAL_PROPERTIES = ['description']
-    DEFAULT_PROPERTIES  = {"dotted":False}
+    DEFAULT_PROPERTIES  = {"dotted":False, "replace":False, "create_dirs":False}
 
     def __init__(self, config):
         self.log = logging.getLogger(__name__)
@@ -48,11 +50,65 @@ class Dot:
             self.log.info("Unexpected fields:")
             self.log.info(config)
 
+    def _check_directory(self):
+        """
+        Checks if the target's directory exists.
+        Will create the directories if specified by 'create_dirs': True.
+        """
+        target_dir = os.path.dirname(self.properties['target'])
+        self.log.debug("Checking for directory {target_dir}...")
+
+        if not os.path.isdir(target_dir):
+            if self.properties['create_dirs']:
+                self.log.debug(f"Directory {target_dir} does not exist. Creating...")
+                os.makedirs(target_dir)
+            else:
+                Wipe(
+                    "NoDirectoryError",
+                    f"The directory {target_dir} for {self.properties['name']} does not exist!",
+                    "Please verify the target file is correct. If you want moar_dots to",
+                    "create the directories for you, include \"'create_dirs': True\" in the config.",
+                )
+
+    def _check_file(self):
+        """
+        Checks if the file exists.
+        Will back up and replace existing file if specified by 'replace': True.
+        """
+        self.log.debug("Checking if the file exists...")
+        if os.path.exists(self.properties["target"]):
+            if self.properties["replace"]:
+                self.log.info(f"File {self.properties['name']} exists. Backing up...")
+                self.log.debug(f"Backing up to {self.properties['target']}{BACKUP_EXTENSION}")
+                os.rename(self.properties["target"], f"{self.properties['target']}{BACKUP_EXTENSION}")
+            else:
+                Wipe(
+                    "FileExistsError",
+                    f"The file {self.properties['name']} already exists! Please verify the",
+                    "target file is correct. If you want this file replaced and backed-up,",
+                    "include \"'replace': True\" in the config.",
+                )
+    
     def dot_it(self):
         """
         Installs the dotfile via symlink and backs up originals
         """
         self.log.info(f"DOT it: {self.properties['name']}")
+
+        self.log.debug("Property Dump:")
+        dump_properties = dict(self.OPTIONAL_PROPERTIES) + \
+                          dict(self.REQUIRED_PROPERTIES) + \
+                          dict(self.DEFAULT_PROPERTIES)
+        dump_properties.pop("name")
+
+        for property in dump_properties:
+            self.log.debug(f"- {property.capitalize()}: {self.properties[property]}")
+
+        self._check_directory()
+        self._check_file()
+
+        os.symlink(self.properties["source"], self.properties["target"])
+    
 
     def nuke_it(self):
         """
